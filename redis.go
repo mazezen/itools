f83fd8f862f5de653ef776776732574a4f2463bf
mazezen <cljcailianjie@gmail.com>
2025-03-13 21:23:38 +0800

    redis update

--- redis.go
+++ redis.go
@@ -3,18 +3,57 @@ package itools
 import (
 	"context"
 	"github.com/redis/go-redis/v9"
+	"os"
 )
 
-// NewRedis 初始化redis 连接
-func NewRedis(addr, password string, db int) (*redis.Client, error) {
-	rdc := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: password,
-		DB:       db,
-	})
-	_, err := rdc.Ping(context.TODO()).Result()
-	if err != nil {
-		return nil, err
+type RedisOption struct {
+	Addr     string `json:"addr"`
+	Password string `json:"password"`
+	DB       int    `json:"db"`
+}
+
+type RedisClientOption func(option *RedisOption)
+
+func RedisClient(option ...RedisClientOption) *RedisOption {
+	r := &RedisOption{}
+	for _, o := range option {
+		o(r)
 	}
-	return rdc, nil
+	return r
+}
+
+func WithAddress(address string) RedisClientOption {
+	return func(o *RedisOption) {
+		o.Addr = address
+	}
+}
+
+func WithRedisPassword(password string) RedisClientOption {
+	return func(o *RedisOption) {
+		o.Password = password
+	}
+}
+
+func WithRedisDB(db int) RedisClientOption {
+	return func(o *RedisOption) {
+		o.DB = db
+	}
+}
+
+func (c *RedisOption) Connect() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     c.Addr,
+		Password: c.Password,
+		DB:       c.DB,
+	})
+	if err := client.Ping(context.Background()).Err(); err != nil {
+		os.Exit(-1)
+	}
+	setClient(client)
+}
+
+var Rc *redis.Client
+
+func setClient(_client *redis.Client) {
+	Rc = _client
 }
