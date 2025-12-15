c1be231187b0ddf7c7dbdad917252fb19ab183db
echo <chhh@sas.com>
2025-12-15 15:27:11 +0800

    feature[slice add Count fucntion]

--- slice.go
+++ slice.go
@@ -325,3 +325,31 @@ func (w *ToolsSlice) PaginateArray(arr []interface{}, page, pageSize int) []inte
 
 	return arr[from:to]
 }
+
+// repeat item in slice. No contain self item.
+func (w *ToolsSlice) Count(arr []interface{}) int {
+n := 0
+	h := 0
+	s := false
+	for c, v := range arr {
+		if s {
+			break
+		}
+		h = 0
+		for i, j := range arr {
+			h++
+			if c == i {
+				continue
+			}
+			if v == j {
+				n++
+			}
+			if h == len(arr) && n >= 1 {
+				s = true
+				break
+			}
+		}
+	}
+
+	return n
+}
